1dcddfca6b282e72b57b9ea18e6026ee2b7a082b
Daniele Pauciulo <danielepach@MacBook-Pro.local>
2017-01-22 16:58:19 +0100

    Facebook authentication

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,38 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("DANY: Unable to authenticate with Facebook - \(error)")
+            }  else if result?.isCancelled == true {
+                print("DANY: User cancelled Facebook authentication")
+            } else {
+                print ("DANY: Successfully authenticated with facebook")
+                
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credential)
+                
+            }
+        }
+        
+    }
+    
+    
+    func firebaseAuth(_ credential: FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("DANY: Unable to authenticate with firebase - \(error)")
+            } else {
+                print("DANY: Successfuly authenticated with Firebase")
+                
+            }
+        })
+    }
 
 }
 
